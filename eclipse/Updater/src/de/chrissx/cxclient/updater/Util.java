05e23d51d1dd34017be5158c9b1982b50ccd771b
chrissx <christian.chrissicx2004@freenet.de>
2018-08-15 11:55:50 +0200

    Finished new Updater, improved and fixed a bit

--- eclipse/Updater/src/de/chrissx/cxclient/updater/Util.java
+++ eclipse/Updater/src/de/chrissx/cxclient/updater/Util.java
@@ -1,24 +1,26 @@
 package de.chrissx.cxclient.updater;
 
 import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.math.BigDecimal;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Util {
 
-	public static Random random = new Random();
+	public static Random rand = new Random(System.currentTimeMillis());
 	static volatile boolean downloading = false;
 	static volatile long total;
 	static volatile UpdaterWindow window = null;
 	static volatile int count = 0;
 	
-	public static void downloadFile(String url, String file, UpdaterWindow window) throws IOException {
+	public static void downloadFile(String url, File file, UpdaterWindow window) {
 		try {
 			URL u = new URL(url);
 			URLConnection c = u.openConnection();
@@ -26,7 +28,7 @@ public class Util {
 			long len = c.getContentLength();
 			BufferedInputStream bis = new BufferedInputStream(u.openStream());
 			FileOutputStream fos = new FileOutputStream(file);
-			final byte data[] = new byte[512];
+			byte data[] = new byte[512];
 			total = 0;
 			downloading = true;
 			Util.window = window;
@@ -60,10 +62,29 @@ public class Util {
 	    }
 	}
 	
-	public static String generateTempFile(String tempPath, String name, String ext) {
-		File f = new File(Paths.get(tempPath, name + "_" + random.nextInt() + ext).toString());
+	public static byte[] downloadBytes(String url) {
+		try {
+			URL u = new URL(url);
+			URLConnection c = u.openConnection();
+			c.connect();
+			long len = c.getContentLength();
+			BufferedInputStream bis = new BufferedInputStream(u.openStream());
+			ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+			byte data[] = new byte[512];
+			while ((count = bis.read(data, 0, data.length)) != -1)
+				bytes.write(data, 0, count);
+			return bytes.toByteArray();
+	    }catch (Exception e) {
+	    	e.printStackTrace();
+	    	return new byte[] { (byte) 0xFA, 0x11 };
+	    }
+	}
+	
+	public static String generateTempFile(String name, String ext) {
+		String tmp = System.getProperty("java.io.tmpdir");
+		File f = new File(Paths.get(tmp, name + "_" + rand.nextLong() + ext).toString());
 		while(f.exists())
-			f = new File(Paths.get(tempPath, name + "_" + random.nextInt() + ext).toString());
+			f = new File(Paths.get(tmp, name + "_" + rand.nextLong() + ext).toString());
 		return f.toString();
 	}
 }
