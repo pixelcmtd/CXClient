efd75ed25105dda7c63d4bf5b461299db195d9c6
chrissx <christian.chrissicx2004@freenet.de>
2018-08-23 00:30:31 +0200

    Added FastBow.

---
+++ src/minecraft/de/chrissx/mods/combat/FastBow.java
@@ -0,0 +1,30 @@
+package de.chrissx.mods.combat;
+
+import de.chrissx.mods.Mod;
+import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.client.multiplayer.PlayerControllerMP;
+import net.minecraft.item.ItemBow;
+import net.minecraft.item.ItemStack;
+import net.minecraft.network.play.client.C03PacketPlayer;
+
+public class FastBow extends Mod {
+
+	public FastBow() {
+		super("FastBow");
+	}
+
+	@Override
+	public void onTick()
+	{
+		EntityPlayerSP p = mc.thePlayer;
+		ItemStack is;
+		if(enabled && mc.gameSettings.keyBindUseItem.isKeyDown() && p.onGround && p.getHealth() > 0 &&
+				(is = p.inventory.getCurrentItem()) != null && is.stackSize > 0 && is.getItem() instanceof ItemBow)
+		{
+			mc.rightClickMouse();
+			for (int i = 0; i < 20; i++)
+			  mc.thePlayer.sendQueue.addToSendQueue(new C03PacketPlayer(false));
+			mc.playerController.onStoppedUsingItem(p);
+		}
+	}
+}
