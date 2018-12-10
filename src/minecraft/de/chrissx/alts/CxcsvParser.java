e688b0fdbaefee68e066275c9ce81561fcf0f71d
chrissx <chrissx@chrissx.ga>
2018-12-10 21:47:54 +0100

    Implemented most of the AltVault backend.

--- src/minecraft/de/chrissx/alts/CxcsvParser.java
+++ src/minecraft/de/chrissx/alts/CxcsvParser.java
@@ -8,19 +8,5 @@ import java.util.List;
 
 public class CxcsvParser {
 
-	/**
-	 * Loads the alts from the given CXColonSeparatedValues-file.
-	 * @param cxcsv The path of the file the CXCSV is located in.
-	 * @return The loaded Alts.
-	 */
-	public static List<Alt> loadAlts(Path cxcsv) throws IOException
-	{
-		List<Alt> alts = new ArrayList<Alt>();
-		for(String s : Files.readAllLines(cxcsv))
-		{
-			String[] t = s.split(":");
-			alts.add(new Alt(t[0], t[1]));
-		}
-		return alts;
-	}
+	
 }
