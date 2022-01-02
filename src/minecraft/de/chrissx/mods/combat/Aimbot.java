ac2d7d3bc6ff9f25a3712d0d0989300851dfe7bb
chris <chrissx@chrissx.de>
2022-01-02 03:51:24 +0100

    converted aimbot

--- src/minecraft/de/chrissx/mods/combat/Aimbot.java
+++ src/minecraft/de/chrissx/mods/combat/Aimbot.java
@@ -3,52 +3,42 @@ package de.chrissx.mods.combat;
 import java.io.File;
 
 import de.chrissx.mods.Mod;
+import de.chrissx.mods.options.Option;
 import de.chrissx.util.Util;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 
 public class Aimbot extends Mod {
 
-	AimbotMode mode = AimbotMode.GUN;
+	Option<AimbotMode> mode = new Option<AimbotMode>("mode", "Gun or bow mode, the style of aiming", AimbotMode.GUN) {
+		@Override
+		public void set(String value) {
+			this.value = value == "" ? (this.value == AimbotMode.BOW ? AimbotMode.GUN : AimbotMode.BOW)
+					: AimbotMode.valueOf(value);
+		}
+	};
 	File mf;
-	
+
 	public Aimbot() {
 		super("AimBot", "aimbot", "Faces the next enemy for shooting with bows, guns, ...");
+		addOption(mode);
 		mf = getApiFile("mode");
 	}
 
 	@Override
 	public void onTick() {
-		if(mode == AimbotMode.GUN)
-			for(Entity e : world().loadedEntityList)
-				if(e instanceof EntityLivingBase && e != player() && !e.isInvisible() && !e.isDead && player().getDistanceToEntity(e) <= 30)
-				{
+		if (mode.value == AimbotMode.GUN)
+			for (Entity e : world().loadedEntityList)
+				if (e instanceof EntityLivingBase && e != player() && !e.isInvisible() && !e.isDead
+						&& player().getDistanceToEntity(e) <= 30) {
 					Util.faceBounds(e.boundingBox);
 					return;
-				}
-		else Util.sendMessage("\u00a74Currently bow mode isn't supported."); // TODO: implement
-	}
-
-	@Override
-	public void onStop() {}
-
-	// TODO: replace
-	@Override
-	public void processCommand(String[] args) {
-		if(args.length == 1)
-			toggle();
-		else if(args.length == 3 && args[1].equalsIgnoreCase("mode"))
-			try {
-				mode = AimbotMode.valueOf(args[2]);
-			} catch (Exception e) {
-				Util.sendMessage("Error valueOf-ing AimbotMode.");
-			}
-		else
-			Util.sendMessage("#aimbot to toggle, #aimbot mode <AimbotMode> to set gun or bow mode.");
+				} else
+					Util.sendError("Currently bow mode isn't supported."); // TODO: implement
 	}
 
 	@Override
 	public void apiUpdate() {
-		write(mf, mode.b);
+		write(mf, mode.value.b);
 	}
 }
